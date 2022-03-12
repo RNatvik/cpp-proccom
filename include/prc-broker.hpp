3945607f6d08d646d5d03569fb88fd17f90c6f40
RNatvik <ronatvik@gmail.com>
2022-03-12 14:04:53 +0100

    Clean slate

--- include/prc-broker.hpp
+++ include/prc-broker.hpp
@@ -6,68 +6,17 @@
 #include <prc-node.hpp>
 
 namespace prc {
-class Broker : public Node {
-   private:
-    std::vector<Endpoint *> nodes;
-    std::map<std::string, std::vector<Endpoint *>> publishedTopics;
-    std::map<std::string, std::vector<Endpoint *>> subscribedTopics;
-    bool shutdown;
+    class Broker {
+    private:
 
-    void run() {
-        while (!this->shutdown) {
-            
-        }
-    }
+        void handleRegister(std::vector<uint8_t>& bytes) {}
+        void sendLinkMessage() {}
+        void optionalPappirmoelle() {}
 
-    void handleRegister(std::vector<uint8_t> &bytes) {
-        RegisterMessage message(bytes);
-
-        Endpoint *target = new Endpoint(message.ip, message.port);
-        bool targetExists = false;
-        for (Endpoint *node : nodes) {
-            targetExists |= *node == *target;
-            if (targetExists) {
-                delete target;
-                target = node;
-                return; // Already registered, do nothing
-            }
-        }
-        nodes.push_back(target);
-        if (message.nodeType == NodeType::SUBSCRIBER) {
-            for (std::string topic : message.topics) {
-                if (subscribedTopics.count(topic) > 0) {
-                    subscribedTopics[topic].push_back(target);
-                } else {
-                    std::vector<Endpoint *> vect;
-                    vect.push_back(target);
-                    subscribedTopics[topic] = vect;
-                }
-                if (publishedTopics.count(topic) > 0) {
-                    auto publishers = publishedTopics[topic];
-                    for (Endpoint *publisher : publishers) {
-                        sendLinkMessage(publisher, target, topic);
-                    }
-                }
-            }
-        } else if (message.nodeType == NodeType::PUBLISHER) {
-            for (std::string topic : message.topics) {
-                if (publishedTopics.count(topic) > 0) {
-                    publishedTopics[topic].push_back(target);
-                } else {
-                    std::vector<Endpoint *> vect;
-                    vect.push_back(target);
-                    publishedTopics[topic] = vect;
-                }
-            }
-        }
-    }
-    void handleHeartbeat(std::vector<uint8_t> &bytes) {}
-    void sendLinkMessage(Endpoint *publisher, Endpoint *subscriber, std::string topic) {}
-
-   public:
-    Broker(std::string ip, uint32_t port): Node(NodeType::BROKER, ip, port) {}
-    ~Broker() {}
-};
+    public:
+        Broker(std::string ip, uint32_t port) {}
+        ~Broker() {}
+    };
 }  // namespace prc
 
 #endif
--- include/prc-node.hpp
+++ include/prc-node.hpp
@@ -2,30 +2,13 @@
 #define PROCCOM_NODE_HPP
 
 #include <prc-common.hpp>
-#include <prc-socket.hpp>
-#include <prc-queue.hpp>
+#include <UdpSocket.hpp>
+#include <map>
+
 
 namespace prc {
 
-
-    /*------------------------------------------------------------------------------
-    Endpoint
-    ------------------------------------------------------------------------------*/
-    struct Endpoint {
-        std::string ip;
-        uint32_t port;
-
-        Endpoint() {}
-        Endpoint(std::string ip, uint32_t port) {
-            this->ip = ip;
-            this->port = port;
-        }
-        ~Endpoint() {}
-
-        bool operator==(const Endpoint& other) {
-            return this->ip == other.ip && this->port == other.port;
-        }
-    };
+    
 
     /*------------------------------------------------------------------------------
     Node
@@ -33,24 +16,39 @@ namespace prc {
     class Node {
     protected:
         bool shutdown;
-        UdpSocket socket;
-        TSQueue<std::vector<uint8_t>> rxQueue;
         bool newMessage;
+        uint64_t heartbeatTimeout;
+        std::string id;
+        soc::UdpSocket socket;
+        soc::TSQueue<std::vector<uint8_t>> rxQueue;
+        std::thread runThread;
+        std::thread admThread;
+
+
 
         void inboundHandler(std::vector<uint8_t>& buffer, std::size_t length, std::string ip, int port) {
-            std::vector<uint8_t> bytes(buffer.begin(), buffer.begin() + length);
-            rxQueue.push_back(bytes);
+            rxQueue.push_back(std::vector<uint8_t>(buffer.begin(), buffer.begin() + length));
             newMessage = true;
         }
 
-        void handleMessage(std::vector<uint8_t>& bytes) {
+        void checkHeartbeats() {
+            auto currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(); // chronos voodoo
+
+        }
+
+        void sendHeartbeats() {
+            auto currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(); // chronos voodoo
+        }
+
+        void handleMessage() {
+            std::vector<uint8_t> bytes = rxQueue.pop_front();
             Message message(bytes);
             switch (message.msgType) {
             case MessageType::REGISTER:
                 this->handleRegister(bytes);
                 break;
             case MessageType::LINK:
-                this->handleLink(bytes);
+                //                this->handleLink(om);
                 break;
             case MessageType::HEARTBEAT:
                 this->handleHeartbeat(bytes);
@@ -63,12 +61,34 @@ namespace prc {
             }
         }
 
-        virtual void handleRegister(std::vector<uint8_t>& bytes) {}
-        virtual void handleLink(std::vector<uint8_t>& bytes) {}
-        virtual void handleHeartbeat(std::vector<uint8_t>& bytes) {}
-        virtual void handlePublish(std::vector<uint8_t>& bytes) {}
+        void handleHeartbeat(std::vector<uint8_t>& bytes) {}
 
-        virtual void run() = 0;
+        virtual void handleRegister(std::vector<uint8_t>& bytes) {} // broker and publisher
+//        virtual void handleLink(OwnedMessage& om) {} // publisher specific
+        virtual void handlePublish(std::vector<uint8_t>& bytes) {} // subscriber specific
+        virtual void optionalPappirmoelle() {}
+        virtual void doRun() = 0;
+
+        void run() {
+            while (!this->shutdown) {
+                if (newMessage) {
+                    newMessage = false;
+                    doRun();
+                }
+            }
+        }
+
+        void pappirmoelle() {
+            using namespace std::chrono_literals;
+            while (!this->shutdown) {
+                std::this_thread::sleep_for(5000ms);
+                this->checkHeartbeats();
+                this->sendHeartbeats();
+
+                optionalPappirmoelle();
+            }
+
+        }
 
     public:
         Node(NodeType type, std::string ip, uint32_t port) :
@@ -79,16 +99,22 @@ namespace prc {
             )
         {
             newMessage = false;
+            shutdown = false;
+            heartbeatTimeout = 30000;
         }
-        ~Node() {}
+        ~Node() { this->stop(); }
 
         void start() {
             this->shutdown = false;
             this->socket.start();
-            this->run(); 
+            this->runThread = std::thread([this]() {this->run();});
+            this->admThread = std::thread([this]() {this->pappirmoelle();});
         }
+
         void stop() {
             this->shutdown = true;
+            if (this->runThread.joinable()) this->runThread.join();
+            if (this->admThread.joinable()) this->admThread.join();
             this->socket.stop();
         }
     };
